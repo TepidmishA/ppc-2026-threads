97927359650baa07a8922a9b8b03d397066d3136
Victoria <vika.kondrashova.05@inbox.ru>
2026-04-17 23:16:44 +0300

    fix tests

--- tasks/kondrashova_v_marking_components/tests/functional/main.cpp
+++ tasks/kondrashova_v_marking_components/tests/functional/main.cpp
@@ -18,6 +18,9 @@ namespace kondrashova_v_marking_components {
 namespace {
 
 int GetExpectedCount(const std::string &type) {
+  if (type == "zero_width" || type == "zero_height") {
+    return 0;
+  }
   if (type == "one_component") {
     return 1;
   }
@@ -33,6 +36,12 @@ int GetExpectedCount(const std::string &type) {
   if (type == "complex") {
     return 2;
   }
+  if (type == "single_row_gaps" || type == "single_column_gaps") {
+    return 2;
+  }
+  if (type == "merge_labels" || type == "boundary_bridge") {
+    return 1;
+  }
   if (type == "large_complex") {
     return 4;
   }
@@ -40,6 +49,9 @@ int GetExpectedCount(const std::string &type) {
 }
 
 bool CheckLabelsSize(const OutType &output_data, const InType &image) {
+  if (image.width <= 0 || image.height <= 0) {
+    return output_data.labels.empty();
+  }
   if (output_data.labels.size() != static_cast<size_t>(image.height)) {
     return false;
   }
@@ -50,6 +62,9 @@ bool CheckLabelsSize(const OutType &output_data, const InType &image) {
 }
 
 bool CheckLabelsValues(const OutType &output_data, const InType &image) {
+  if (image.width <= 0 || image.height <= 0) {
+    return true;
+  }
   for (int ii = 0; ii < image.height; ++ii) {
     for (int jj = 0; jj < image.width; ++jj) {
       auto idx = (static_cast<size_t>(ii) * static_cast<size_t>(image.width)) + static_cast<size_t>(jj);
@@ -65,6 +80,41 @@ bool CheckLabelsValues(const OutType &output_data, const InType &image) {
     }
   }
   return true;
+}
+
+// 4 components to defeat GCC -O3 unrolling (kept out of GetTestInputData for clang-tidy complexity).
+InType MakeLargeComplexTestImage() {
+  constexpr int kSize = 30;
+  InType image{};
+  image.width = kSize;
+  image.height = kSize;
+  image.data.assign(static_cast<size_t>(kSize) * static_cast<size_t>(kSize), 1);
+
+  for (int i = 0; i < kSize; ++i) {
+    image.data[static_cast<size_t>(i)] = 0;
+  }
+
+  for (int i = 5; i < 25; ++i) {
+    image.data[(i * kSize) + 15] = 0;
+  }
+
+  for (int i = 25; i <= 27; ++i) {
+    for (int j = 25; j <= 27; ++j) {
+      image.data[(i * kSize) + j] = 0;
+    }
+  }
+
+  for (int i = 10; i < 20; ++i) {
+    image.data[(i * kSize) + 5] = 0;  // left arm
+  }
+  for (int i = 10; i < 20; ++i) {
+    image.data[(i * kSize) + 10] = 0;  // right arm
+  }
+  for (int j = 5; j <= 10; ++j) {
+    image.data[(19 * kSize) + j] = 0;  // bottom connection
+  }
+
+  return image;
 }
 
 }  // namespace
@@ -96,7 +146,13 @@ class MarkingComponentsFuncTest : public ppc::util::BaseRunFuncTests<InType, Out
     const std::string &type = std::get<1>(GetParam());
     InType image{};
 
-    if (type == "empty") {
+    if (type == "zero_width") {
+      image.width = 0;
+      image.height = 3;
+    } else if (type == "zero_height") {
+      image.width = 3;
+      image.height = 0;
+    } else if (type == "empty") {
       image.data = {1, 1, 1, 1, 1, 1, 1, 1, 1};
       image.width = 3;
       image.height = 3;
@@ -121,39 +177,33 @@ class MarkingComponentsFuncTest : public ppc::util::BaseRunFuncTests<InType, Out
       image.data = {0, 1, 0, 1, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1};
       image.width = 5;
       image.height = 5;
+    } else if (type == "single_row_gaps") {
+      image.data = {0, 0, 1, 0, 0};
+      image.width = 5;
+      image.height = 1;
+    } else if (type == "single_column_gaps") {
+      image.data = {0, 0, 1, 0, 0};
+      image.width = 1;
+      image.height = 5;
+    } else if (type == "merge_labels") {
+      image.data = {0, 1, 0,
+                    0, 0, 0,
+                    1, 1, 1};
+      image.width = 3;
+      image.height = 3;
+    } else if (type == "boundary_bridge") {
+      image.data = {1, 0, 1,
+                    1, 0, 1,
+                    1, 0, 1,
+                    1, 0, 1,
+                    1, 0, 1,
+                    1, 0, 1,
+                    1, 0, 1,
+                    1, 0, 1};
+      image.width = 3;
+      image.height = 8;
     } else if (type == "large_complex") {
-      // 4 components to defeat GCC -O3 unrolling
-      image.width = 30;
-      image.height = 30;
-      image.data.assign(30 * 30, 1);
-
-      // Component 1: top horizontal line
-      for (int i = 0; i < 30; ++i) {
-        image.data[0 * 30 + i] = 0;
-      }
-
-      // Component 2: vertical line in middle
-      for (int i = 5; i < 25; ++i) {
-        image.data[i * 30 + 15] = 0;
-      }
-
-      // Component 3: box
-      for (int i = 25; i <= 27; ++i) {
-        for (int j = 25; j <= 27; ++j) {
-          image.data[i * 30 + j] = 0;
-        }
-      }
-
-      // Component 4: U-shape
-      for (int i = 10; i < 20; ++i) {
-        image.data[i * 30 + 5] = 0;  // left arm
-      }
-      for (int i = 10; i < 20; ++i) {
-        image.data[i * 30 + 10] = 0;  // right arm
-      }
-      for (int j = 5; j <= 10; ++j) {
-        image.data[19 * 30 + j] = 0;  // bottom connection
-      }
+      image = MakeLargeComplexTestImage();
     }
 
     return image;
@@ -165,10 +215,14 @@ TEST_P(MarkingComponentsFuncTest, VariousBinaryImages) {
   ExecuteTest(GetParam());
 }
 
-const std::array<TestType, 7> kTestParam = {std::make_tuple(0, "empty"),           std::make_tuple(1, "one_component"),
-                                            std::make_tuple(2, "isolated_pixels"), std::make_tuple(3, "two_regions"),
-                                            std::make_tuple(4, "u_shape"),         std::make_tuple(5, "complex"),
-                                            std::make_tuple(6, "large_complex")};
+const std::array<TestType, 13> kTestParam = {
+    std::make_tuple(0, "zero_width"),        std::make_tuple(1, "zero_height"),
+    std::make_tuple(2, "empty"),             std::make_tuple(3, "one_component"),
+    std::make_tuple(4, "isolated_pixels"),   std::make_tuple(5, "two_regions"),
+    std::make_tuple(6, "u_shape"),           std::make_tuple(7, "complex"),
+    std::make_tuple(8, "single_row_gaps"),   std::make_tuple(9, "single_column_gaps"),
+    std::make_tuple(10, "merge_labels"),     std::make_tuple(11, "boundary_bridge"),
+    std::make_tuple(12, "large_complex")};
 
 const auto kTestTasksList = std::tuple_cat(
     ppc::util::AddFuncTask<KondrashovaVTaskSEQ, InType>(kTestParam, PPC_SETTINGS_kondrashova_v_marking_components),
